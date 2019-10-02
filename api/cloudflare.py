a683130b78adf6575a0e3692404568c8aa137acc
lennardkittner <lennard599web.de>
2019-10-02 13:51:36 +0200

    zoneID and recordID caching

--- api/cloudflare.py
+++ api/cloudflare.py
@@ -1,9 +1,33 @@
 # import requests
+import yaml
 from .curl import get, put
 
-# if not options["silent"]:
+def load_cache(filepath, zone):
+  try:
+    with open(filepath, 'r') as cache_r
+    c = yaml.load(cache_r, Loader=yaml.Loader)
+    if zone in c:
+      return c[zone]
+  except FileNotFoundError:
+    if options["verbose"]: print('UPDATE: Cache file not found')
 
 def get_zones(options, domain):
+
+
+  # if "zoneID" in load_cache('.dd-update-cache.yml', domain["zone"]):
+  #   return d["zoneID"]
+  
+  try:
+    with open(".dd-update-cache.yml", "r") as cache_r:
+      c = yaml.load(cache_r, Loader=yaml.Loader)
+      if domain["zone"] in c: 
+        d = c[domain["zone"]]
+        if "zoneID" in d:
+          return d["zoneID"]
+
+  except FileNotFoundError:
+    if options["verbose"]: print("UPDATE: Cache file not found")
+   
   url = domain["server"] + '/zones?name=' + domain["zone"] + '&status=active&page=1&per_page=1'
 
   if options["verbose"]:
@@ -24,10 +48,25 @@ def get_zones(options, domain):
     print("UPDATE: something went wrong (get_zones)", json["errors"])
   if options["verbose"]: print("UPDATE: RESPONSE:", json)
 
+  c.update({domain["zone"] : {'zoneID' : json["result"][0]["id"]}})
+  cache_w = open(".dd-update-cache.yml", "w")
+  yaml.dump(c, cache_w, default_flow_style=False)
+
   return json["result"][0]["id"]
 
 
 def get_records(options, domain, zone_id):
+  try:
+    with open(".dd-update-cache.yml", "r") as cache_r:
+      c = yaml.load(cache_r, Loader=yaml.Loader)
+      if domain["zone"] in c: 
+        d = c[domain["zone"]]
+        if "recordID" in d:
+          return d["recordID"]
+
+  except FileNotFoundError:
+    if options["verbose"]: print("UPDATE: Cache file not found")
+   
   url = domain["server"] + '/zones/' + zone_id + '/dns_records?type=A&name=' + domain["zone"] + '&page=1&per_page=1'
 
   if options["verbose"]:
@@ -47,6 +86,10 @@ def get_records(options, domain, zone_id):
   if json["success"] == False and options["verbose"]:
     print("UPDATE: something went wrong (get_records)", json["errors"])
   if options["verbose"]: print("UPDATE: RESPONSE:", json)
+
+  c.update({domain["zone"] : {'recordID' : json["result"][0]["id"], 'zoneID' : zone_id}})
+  cache_w = open(".dd-update-cache.yml", "w")
+  yaml.dump(c, cache_w, default_flow_style=False)
 
   return json["result"][0]["id"]
 
